5c9c2a2907f06f76117b758002585b35612f9e20
Alejandro Rodriguez <alejandrorodriguez@Alejandros-MBP.home>
2023-06-02 20:08:01 +0100

    first commit

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,43 +1,68 @@
 <template>
   <Page>
-    <ActionBar>
-      <Label text="Home"/>
-    </ActionBar>
+    <ActionBar title="Native Flex" />
 
-    <GridLayout>
-      <Label class="info">
-        <FormattedString>
-          <Span class="fas" text.decode="&#xf135; "/>
-          <Span :text="message"/>
-        </FormattedString>
-      </Label>
-    </GridLayout>
+    <ListView height="100%" separatorColor="transparent" for="item in flicks" @itemTap="onFlickTap">
+      <v-template>
+        <GridLayout
+          height="200"
+          borderRadious="10"
+          class="bg-secondary"
+          rows="*, auto, auto"
+          columns="*"
+          margin="5 10"
+          padding="0"
+        >
+          <Image row="0" margin="0" :src="item.image" stretch="aspectFill" />
+          <Label
+            row="1"
+            :text="item.title"
+            margin="10 10 0 10"
+            fontWeight="700"
+            class="text-primary"
+            fontSize="18"
+          />
+          <Label
+            row="2"
+            margin="0 10 10 10"
+            class="text-secondary"
+            fontSize="14"
+            textWrap="true"
+            :text="item.description"
+          />
+        </GridLayout>
+
+      </v-template>
+
+    </ListView>
+  
+
   </Page>
 </template>
 
 <script lang="ts">
-  import Vue from "nativescript-vue";
+import Vue from "nativescript-vue";
+import { FlickModel } from "~/models/Flick";
+import FlickService from "~/services/FlickService";
+import Details from "./Details.vue";
 
-  export default Vue.extend({
-    computed: {
-      message() {
-        return "Blank {N}-Vue app";
-      }
+const flickService: FlickService = new FlickService();
+
+export default Vue.extend({
+  data() {
+    return {
+      text: '',
+      flicks: flickService.getFlicks(),
+    };
+  },
+  methods:{
+    onFlickTap(args:any){
+        const flick = args.item as FlickModel;
+        this.$navigateTo(Details,{
+            transitioniOS: {name: "fade"},
+            props: {id: flick.id}
+        });
     }
-  });
+  }
+});
 </script>
-
-<style scoped lang="scss">
-  @import '@nativescript/theme/scss/variables/blue';
-
-  // Custom styles
-  .fas {
-    @include colorize($color: accent);
-  }
-
-  .info {
-    font-size: 20;
-    horizontal-align: center;
-    vertical-align: center;
-  }
-</style>
